fd4fcdb36554d07b06a848a1a62e99ce646f295f
Your Name <you@example.com>
2024-04-16 10:06:10 -0600

    tabs

--- ManejoTabs.js
+++ ManejoTabs.js
@@ -2,25 +2,45 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Usuario from './Usuario';
 import Usuario1 from './Usuario1';
 import Usuario2 from './Usuario2';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
 const ManejoTabs = props => {
-  const Tab = createBottomTabNavigator();
+  const Tab = createMaterialBottomTabNavigator();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      activeColor="#865ba0"
+      inactiveColor="#747d84"
+      barStyle={{backgroundColor: '#161d25'}}>
       <Tab.Screen
-        name="tab1"
+        name="Imagen"
         component={Usuario}
         initialParams={{nombre: props.route.params.nombre}}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="image" color={'#747d84'} size={25} />
+          ),
+        }}
       />
       <Tab.Screen
-        name="tab2"
+        name="Video"
         component={Usuario1}
         initialParams={{nombre: props.route.params.nombre}}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="video" color={'#747d84'} size={25} />
+          ),
+        }}
       />
       <Tab.Screen
-        name="tab3"
+        name="Sonido"
         component={Usuario2}
         initialParams={{nombre: props.route.params.nombre}}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="speaker" color={'#747d84'} size={25} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
